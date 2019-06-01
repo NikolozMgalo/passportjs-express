a1a1378dd6b0f1c27c51a7476a99181ba356379b
nikolozmgalo <nikolozmgalo@gmail.com>
2019-06-01 22:58:41 +0400

    prettier

--- app.js
+++ app.js
@@ -1,61 +1,66 @@
-const express = require("express")
-const path = require('path')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const cors = require("cors")
-const mongoose = require("mongoose")
-const errorHandler = require("errorhandler")
-const db = require('./db.js')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+const db = require('./db.js');
 
+mongoose.promise = global.Promise;
 
-mongoose.promise = global.Promise
-
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production';
 
 const app = express();
 
-app.use(cors())
-app.use(require('morgan')('dev'))
-app.use(bodyParser.urlencoded({extended : true}))
-app.use(bodyParser.json())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(session({secret : 'passport', coockie: {maxAge: 6000}, resave: false, saveUninitialized: false }))
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'passport',
+    coockie: { maxAge: 6000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
-if(!isProduction){
-    app.use(errorHandler())
+if (!isProduction) {
+  app.use(errorHandler());
 }
 
-mongoose.connect(db, { useNewUrlParser : true })
-mongoose.set('debug', true)
+mongoose.connect(db, { useNewUrlParser: true });
+mongoose.set('debug', true);
 
-require('./model/user.model')
-require('./config/passport')
-app.use(require('./routes'))
+require('./model/user.model');
+require('./config/passport');
+app.use(require('./routes'));
 
-
-if(!isProduction){
-    app.use((err, req, res, next) => {
-        res.status(err.status || 500);
-        console.log(err)
-
-        res.json({
-            errors:{
-                message: err.message,
-                err:err,
-            },
-        })
-    })
-}
-app.use((err, req, res, next) => {
-    res.status(err.status || 500)
-    console.log(err)
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.log(err);
 
     res.json({
-        errors: {
-            message: err.message,
-            err: {message: "something went wrong"},
-        }
-    })
-})
+      errors: {
+        message: err.message,
+        err: err
+      }
+    });
+  });
+}
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  console.log(err);
 
-app.listen(3000, () => console.log("server is running port is 3000"))
+  res.json({
+    errors: {
+      message: err.message,
+      err: { message: 'something went wrong' }
+    }
+  });
+});
+
+app.listen(3000, () => console.log('server is running port is 3000'));
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
-const express = require('express')
+const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/users', require('./users'))
+router.use('/users', require('./users'));
 
-module.exports = router
+module.exports = router;
